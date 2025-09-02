7afbd7bc1426d0d6a007b7bd10274ce272e28106
jhari3099 <u202018836@upc.edu.pe>
2025-09-02 02:07:06 -0500

    solucionar errores .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,4 +7,5 @@ export const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT // <-- Agrega esta línea
 });
